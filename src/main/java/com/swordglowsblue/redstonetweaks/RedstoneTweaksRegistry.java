eb45ecf431b71fc7ffee1fe078c34b0cb1031aa4
Connor Scialdone <cj@swordglowsblue.com>
2019-05-31 22:46:36 -0600

    Add creative tab

--- src/main/java/com/swordglowsblue/redstonetweaks/RedstoneTweaksRegistry.java
+++ src/main/java/com/swordglowsblue/redstonetweaks/RedstoneTweaksRegistry.java
@@ -5,8 +5,13 @@ import com.swordglowsblue.redstonetweaks.block.analog.AnalogRedstoneBlock;
 import com.swordglowsblue.redstonetweaks.block.analog.AnalogRedstoneLampBlock;
 import com.swordglowsblue.redstonetweaks.item.FlintAndRedstoneItem;
 import com.swordglowsblue.redstonetweaks.util.RegistryUtils;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 
 public class RedstoneTweaksRegistry implements RegistryUtils {
+    public final ItemGroup itemGroupRTweaks;
+
     public final AnalogRedstoneBlock analogRedstoneBlock;
     public final AnalogRedstoneLampBlock analogRedstoneLamp;
     public final RedstoneSparkBlock redstoneSpark;
@@ -18,5 +23,14 @@ public class RedstoneTweaksRegistry implements RegistryUtils {
         analogRedstoneLamp = registerBlock("analog_redstone_lamp", new AnalogRedstoneLampBlock());
         redstoneSpark = registerLoneBlock("redstone_spark", new RedstoneSparkBlock());
         flintAndRedstone = registerItem("flint_and_redstone", new FlintAndRedstoneItem());
+        itemGroupRTweaks = FabricItemGroupBuilder.create(ID("main"))
+            .icon(() -> new ItemStack(flintAndRedstone))
+            .appendItems(RegistryUtils.itemGroupContents((blocks, items) -> {
+                blocks.add(analogRedstoneBlock);
+                blocks.add(analogRedstoneLamp);
+
+                items.add(flintAndRedstone);
+            }))
+            .build();
     }
 }
