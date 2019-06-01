53f3b3aad4ba016b123db8025b429b2b6c941d3a
Connor Scialdone <cj@swordglowsblue.com>
2019-05-31 22:49:18 -0600

    Add missing blockstate gen for analog blocks/lamps

--- src/gen_resources/blockstates.js
+++ src/gen_resources/blockstates.js
@@ -1,5 +1,14 @@
 const gen = require('./gen')
 const def = require('./def')
 
+function genAnalogBlockstates() {
+  gen.ensureDir(gen.assets, 'blockstates')
+  for(let type of def.analogTypes) {
+    gen.write(gen.assets, `blockstates/analog_redstone_${type}.json`,
+      gen.template(`blockstates/analog`).replace(/%type/g, type))
+  }
+}
+
 module.exports = [
+  genAnalogBlockstates,
 ]
